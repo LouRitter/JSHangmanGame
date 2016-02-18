8c1d756a71849bf5b9e7bef4375435cd0d7ba2e0
LouRitter <louritter1@gmail.com>
2016-02-17 21:58:13 -0500

    unfinished hangman

--- game.js
+++ game.js
@@ -1,43 +1,75 @@
 
 window.onload = function()
 {
-    
+  
 var words = ["space", "astronomy", "comet", "planet", "earth", "saturn", "star","telescope", "uranus", "spaceship", "astronaut","meteorite"];
+var win=0
+var loss=0;
+
+//reset
+var lettersGuessed=[];
 var x =Math.floor(Math.random() * words.length);
 		console.log	(words[x]);
+var guessesLeft=10;
+	console.log(guessesLeft);
+var dashLength="";
 var dashes= " _ ";
 	for (var i = 0; i < words[x].length; i++) {
-		console.log(dashes);
-};
+		dashLength = dashLength + dashes;
+		console.log(dashLength);
 
-var guessesLeft=5;
-	console.log(guessesLeft);
+};
+document.getElementById("word").innerHTML = dashLength;
+ //reset
+function reset () {
+	// body...
+}
+ var lettersGuessed=[];
+var x =Math.floor(Math.random() * words.length);
+		console.log	(words[x]);
+var guessesLeft=10;
+	
+var dashLength="";
+var dashes= " _ ";
+	for (var i = 0; i < words[x].length; i++) {
+		dashLength = dashLength + dashes;
+		console.log(dashLength);
+
+};
+document.getElementById("word").innerHTML = dashLength;	
+
+
 
 document.onkeyup = function(event) {
-	console.log(guessesLeft--);
-
+	
+	guessesLeft--;
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 		search();
+		lettersGuessed.push(userGuess);
 			function search() {
 				
-				for (var j = 0; j < words[x].length; j++) {
+				for (var j = 0; j < words[j].length; j++) {
 
-				if (userGuess == words[x].charAt(j)){
+				if (userGuess == words[j].charAt(j)){
 					alert("works");
-					guessesLeft=guessesLeft+1;
+					userGuess.charAt(words[j])=dashLength(userGuess);
 					
 				}else{
-						
-				}
-		
+						guessesleft=guessesLeft-1;
+						}
+							if (guessesLeft == 0){
+							reset();
+							loss++
+							};
+					
+							
 
 };
-	
+document.getElementById("guess").innerHTML = guessesLeft;
+document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+document.getElementById("loses").innerHTML = loss;
 };
-
 };
-document.getElementById("word").innerHTML= dashes;
-document.getElementById("guess").innerHTML= guessesLeft;
 };
 
 // var substring= words[x].substring();
